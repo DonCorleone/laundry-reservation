42b33e26e600ca3d599a7e64ff7866a472f80933
linus <linus@wieland.click>
2023-12-24 18:29:37 +0100

    remove unused Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,15 @@ import {BrowserModule, HammerModule} from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatLegacySliderModule as MatSliderModule } from '@angular/material/legacy-slider';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
-import { MatLegacyCheckboxModule as MatCheckboxModule } from '@angular/material/legacy-checkbox';
 import { FormsModule } from '@angular/forms';
-import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
-import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
-import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core'
 import {
   MatSidenavContainer,
   MatSidenavModule,
 } from '@angular/material/sidenav';
-import { MatToolbar, MatToolbarModule } from '@angular/material/toolbar';
-import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatLegacyProgressSpinnerModule as MatProgressSpinnerModule } from '@angular/material/legacy-progress-spinner';
-import { MatLegacyTabsModule as MatTabsModule } from '@angular/material/legacy-tabs';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { HourComponent } from './hour/hour.component';
@@ -29,16 +20,27 @@ import { CalendarComponent } from './calendar/calendar.component';
 import { ScrollManagerDirective } from './directives/scroll-manager.directive';
 import { ScrollSectionDirective } from './directives/scroll-section.directive';
 import { ScrollAnchorDirective } from './directives/scroll-anchor.directive';
+import {MatCard, MatCardModule} from "@angular/material/card";
 
 @NgModule({
+  declarations: [AppComponent, HourComponent, HourHeaderComponent, CalendarComponent, ScrollManagerDirective, ScrollSectionDirective, ScrollAnchorDirective],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatGridListModule,
     FormsModule,
+    MatCardModule,
+    MatRippleModule,
+    MatSidenavModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatExpansionModule,
     HammerModule
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-
 export class AppModule {}
