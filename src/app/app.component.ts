ef13ed27c023d16b15dda70b272fcecfadce85e7
Linus Wieland <linus@wieland.click>
2024-08-20 20:41:46 +0200

    remove viewchild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
-import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
-import {Subscription} from 'rxjs';
-import {DayService, cellType, Tile} from './services/day.service';
-import {hour} from './models/hour';
-import {MatGridListModule} from "@angular/material/grid-list";
-import {CalendarComponent} from "./calendar/calendar.component";
-import {ScrollManagerDirective} from "./directives/scroll-manager.directive";
-import {CommonModule} from "@angular/common";
-import {HourHeaderComponent} from "./hour-header/hour-header.component";
-import {HourComponent} from "./hour/hour.component";
-import {ScrollSectionDirective} from "./directives/scroll-section.directive";
-import {ScrollAnchorDirective} from "./directives/scroll-anchor.directive";
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DayService, cellType, Tile } from './services/day.service';
+import { hour } from './models/hour';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { CalendarComponent } from './calendar/calendar.component';
+import { ScrollManagerDirective } from './directives/scroll-manager.directive';
+import { CommonModule } from '@angular/common';
+import { HourHeaderComponent } from './hour-header/hour-header.component';
+import { HourComponent } from './hour/hour.component';
+import { ScrollSectionDirective } from './directives/scroll-section.directive';
+import { ScrollAnchorDirective } from './directives/scroll-anchor.directive';
 
 @Component({
   selector: 'app-root',
@@ -17,10 +17,15 @@ import {ScrollAnchorDirective} from "./directives/scroll-anchor.directive";
   standalone: true,
   providers: [ScrollSectionDirective, ScrollManagerDirective],
   imports: [
-    CommonModule, CalendarComponent, MatGridListModule,
-    HourHeaderComponent, HourComponent, ScrollSectionDirective,
-    ScrollAnchorDirective, ScrollManagerDirective,
-  ]
+    CommonModule,
+    CalendarComponent,
+    MatGridListModule,
+    HourHeaderComponent,
+    HourComponent,
+    ScrollSectionDirective,
+    ScrollAnchorDirective,
+    ScrollManagerDirective,
+  ],
 })
 export class AppComponent implements OnDestroy {
   title = 'laundry';
@@ -32,16 +37,11 @@ export class AppComponent implements OnDestroy {
   eventText = '';
   private subscription: Subscription;
 
-  @ViewChild('calendarElement', { static: false })
-  calendarElement: ElementRef;
-
   constructor(private dayService: DayService) {
-
     this.subscription = this.dayService.tiles$.subscribe(
       (x) => (this.tiles = x)
     );
   }
-
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
@@ -58,10 +58,10 @@ export class AppComponent implements OnDestroy {
 
   clickMachineColumn($event: MouseEvent, machine: string) {
     this.tiles
-          .filter((t) => t.hour && t.machine == machine)
-          .forEach((t) => {
-            t.hour.selectedBy = 'zzz';
-            console.log(t.hour.end, t.hour.begin, t.hour.selectedBy);
-          });
+      .filter((t) => t.hour && t.machine == machine)
+      .forEach((t) => {
+        t.hour.selectedBy = 'zzz';
+        console.log(t.hour.end, t.hour.begin, t.hour.selectedBy);
+      });
   }
 }
