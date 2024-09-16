1f5c586894d96b51374a1c0d543b841de7105759
Linus Wieland <linus@wieland.click>
2024-09-16 22:19:37 +0200

    netlify

--- src/app/services/reservation.service.ts
+++ src/app/services/reservation.service.ts
@@ -6,8 +6,8 @@ import {ReservationEntry} from "../models/reservation-entry";
   providedIn: 'root'
 })
 export class ReservationService {
-  private baseUrlLocal = 'http://localhost:5263';
-  private baseUrlRender = 'https://laundrysignalr-init.onrender.com/api/Reservation';
+  // private baseUrlLocal = 'http://localhost:5263';
+  private baseUrlRender = 'https://laundrysignalr-init.onrender.com';
 
   constructor(private httpClient: HttpClient) {}
 
@@ -20,12 +20,12 @@ export class ReservationService {
       customParams.machineids.map((id, index) => [`machineid${index}`, id])
     ).toString();
 
-    return this.httpClient.get<ReservationEntry[]>(`${this.baseUrlLocal}/api/ReservationEntries?${queryString}`);
+    return this.httpClient.get<ReservationEntry[]>(`${this.baseUrlRender}/api/ReservationEntries?${queryString}`);
   }
 
   public addReservation(reservationEntry: ReservationEntry) {
 
-    this.httpClient.post<ReservationEntry>(`${this.baseUrlLocal}/api/ReservationEntries`, reservationEntry).subscribe(reservation => {
+    this.httpClient.post<ReservationEntry>(`${this.baseUrlRender}/api/ReservationEntries`, reservationEntry).subscribe(reservation => {
       console.log('Updated reservation:', reservation);
     });
   }
@@ -36,7 +36,7 @@ export class ReservationService {
     const options = {
       body: reservationEntry,
     };
-    this.httpClient.delete<string>(`${this.baseUrlLocal}/api/ReservationEntries`, options).subscribe(reservationId => {
+    this.httpClient.delete<string>(`${this.baseUrlRender}/api/ReservationEntries`, options).subscribe(reservationId => {
       console.log('Deleted reservation:', reservationId);
     });
   }
--- src/app/services/signalr.service.ts
+++ src/app/services/signalr.service.ts
@@ -10,8 +10,8 @@ import {BehaviorSubject, Observable, of} from "rxjs";
 export class SignalRService {
   private hubConnection: signalR.HubConnection;
   private reservationEntries = signal<ReservationEntry[]>([]); // Signal to store messages
-  private baseUrlLocal = 'http://localhost:5263';
-  private baseUrlRender = 'https://laundrysignalr-init.onrender.com/api/ReservationEntries';
+ // private baseUrlLocal = 'http://localhost:5263';
+  private baseUrlRender = 'https://laundrysignalr-init.onrender.com';
   hourPerDate = signal<Map<string, number>>(null);
   updatedReservation = new BehaviorSubject<Record<string, string> | null>(null);
   updatedReservation$: Observable< Record<string, string> | null> = this.updatedReservation.asObservable();
@@ -26,7 +26,7 @@ export class SignalRService {
     ).toString();
 
     this.hubConnection = new signalR.HubConnectionBuilder()
-      .withUrl(`${this.baseUrlLocal}/hub?${queryString}`, {
+      .withUrl(`${this.baseUrlRender}/hub?${queryString}`, {
         withCredentials: true,
       })
       .build();
