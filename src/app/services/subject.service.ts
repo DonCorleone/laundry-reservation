97020965db9f3b7bf1585ac22011371de7ff9db1
linus <linus@wieland.click>
2024-10-05 23:29:49 +0200

    prod mode

--- src/app/services/subject.service.ts
+++ src/app/services/subject.service.ts
@@ -1,20 +1,23 @@
-import {Injectable} from '@angular/core';
+import {Injectable, isDevMode} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {ISubject} from "../models/subject";
 import {BehaviorSubject, take} from "rxjs";
 
-const baseUrl = 'http://localhost:3000'; // json-server
-// const baseUrl = 'http://localhost:5263'; // dotNet
-// const baseUrl = 'https://laundrysignalr-init.onrender.com'; // render
 @Injectable({
   providedIn: 'root'
 })
 export class SubjectService {
+
+  private baseUrl = 'https://laundrysignalr-init.onrender.com'; // render
   private _subjects = new BehaviorSubject<ISubject[] | null>(null);
   subjects$ = this._subjects.asObservable();
 
   constructor(private httpClient: HttpClient) {
-    this.httpClient.get<ISubject[]>(`${baseUrl}/api/Subjects`).pipe(
+    if (isDevMode()) {
+      this.baseUrl = 'http://localhost:3000'; // json-server
+      this.baseUrl = 'http://localhost:5263'; // dotNet
+    }
+    this.httpClient.get<ISubject[]>(`${this.baseUrl}/api/Subjects`).pipe(
       take(1)).subscribe(subjects => this._subjects.next(subjects))
   }
 }
