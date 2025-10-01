5972d81c64b62aa2142fc916bc0c9f5825073b52
doncorleone <linus@wieland.click>
2025-10-01 22:14:32 +0200

    dynamic callback

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
   tenantCode: 'default',
   backendUrl: 'https://laundrysignalr-mongodb.onrender.com',
   auth0: {
-    domain: 'laundry-calendar.eu.auth0.com', // <-- replace with actual value
-    clientId: 'zjmCa3vVQA11RsNkb44RdE5qqAy9hGnh',   // <-- replace with actual value
-    baseUrl: 'https://slotwi.se/callback'
-  }
+    domain: 'laundry-calendar.eu.auth0.com',
+    clientId: 'zjmCa3vVQA11RsNkb44RdE5qqAy9hGnh',
+    baseUrl: window.location.origin + '/callback',
+  },
 };
