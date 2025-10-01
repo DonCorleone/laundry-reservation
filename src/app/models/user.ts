72db9576723fd5df5b6c6e3feb39bcaf09ea3414
doncorleone <linus@wieland.click>
2025-10-01 16:28:13 +0200

    remove netlify identity

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,5 +1,14 @@
-import {User} from "netlify-identity-widget";
+export interface User {
+  sid: string;
+  nickname: string;
+  name: string;
+  picture: string;
+  updated_at: string;
+  email: string;
+  sub: string;
+}
 
 export interface ILaundryUser extends User {
   key: string;
+  tenant?: string; // Optional tenant code from server config
 }
